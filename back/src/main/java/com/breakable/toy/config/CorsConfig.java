4c85c3c55ef741bb6a24528fdd625f064f147fee
Milton Gonzalez <milton.gonzalez@encora.com>
2024-12-30 17:00:00 -0600

    feat: Adds Cors configuration.

---
+++ back/src/main/java/com/breakable/toy/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.breakable.toy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        final String commonOrigin = "http://localhost:3000";
+        registry.addMapping("/products/**")
+                .allowedOrigins(commonOrigin)
+                .allowedMethods("GET", "POST", "PUT");
+    }
+}
