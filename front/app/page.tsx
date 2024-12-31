aae00e47b75e62aebd5d78671b018a189a4fe72d
Milton Gonzalez <milton.gonzalez@encora.com>
2024-12-30 19:02:03 -0600

    feat: Implement basic fetching using axios.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,116 +1,84 @@
+"use client";
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 import { Button, Stack } from "@mui/material";
 import { SearchMenu } from "@/components/SearchMenu";
 import { Statistics } from "@/components/Statistics";
-
-const rows: GridRowsProp = [
-  {
-    id: 1,
-    col1: "Food",
-    col2: "Watermelon",
-    col3: 123,
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 2,
-    col1: "Food",
-    col2: "Eatermelon",
-    col3: 234,
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 3,
-    col1: "Food",
-    col2: "WTtermelon",
-    col3: 234,
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 4,
-    col1: "Food",
-    col2: "rGtermelon",
-    col3: 23,
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 5,
-    col1: "Food",
-    col2: "Wrtermelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 6,
-    col1: "Food",
-    col2: "Wsdermelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 7,
-    col1: "Food",
-    col2: "Watsdmelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 8,
-    col1: "Food",
-    col2: "Watermelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 9,
-    col1: "Food",
-    col2: "Wasdfmelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 10,
-    col1: "Food",
-    col2: "Watermelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-  {
-    id: 11,
-    col1: "Food",
-    col2: "Wsdfhmelon",
-    col3: "1.50",
-    col4: "12/25/2024",
-    col5: "50",
-  },
-];
+import { AxiosInstance } from "@/utils/axiosInstance";
+import React, { useEffect } from "react";
 
 const columns: GridColDef[] = [
-  { field: "col1", headerName: "Category", width: 150 },
-  { field: "col2", headerName: "Name", width: 150 },
-  { field: "col3", headerName: "Price", width: 150 },
-  { field: "col4", headerName: "Expiration Date", width: 200 },
-  { field: "col5", headerName: "Stock" },
+  { field: "category", headerName: "Category", width: 150 },
+  { field: "name", headerName: "Name", width: 150 },
+  { field: "price", headerName: "Price", width: 150 },
+  { field: "expDate", headerName: "Expiration Date", width: 200 },
+  { field: "inStock", headerName: "Quantity in Stock", minWidth: 200 },
 ];
 
-const categories = ["Food", "Cleaning", "Misc"];
+const parseProducts: any = (products: Product[]) => {
+  let parsed: any = [];
+
+  products.map((product) => {
+    parsed.push({
+      id: product.id,
+      category: product.category,
+      name: product.name,
+      price: product.unitPrice,
+      expDate: product.expirationDate,
+      inStock: product.quantityInStock,
+    });
+  });
+
+  return parsed;
+};
+
+type Product = {
+  category: string;
+  creationDate: Date;
+  expirationDate?: Date;
+  id: string;
+  name: string;
+  quantityInStock: number;
+  unitPrice: number;
+  updateDate: Date;
+};
 
 export default function Home() {
+  const [products, setProducts] = React.useState(null);
+  const [categories, setCategories] = React.useState<string[]>([]);
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
+  const getProducts = () => {
+    AxiosInstance.get("/products")
+      .then((response) => {
+        setProducts(parseProducts(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const getCategories = () => {
+    AxiosInstance.get("/products/categories")
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div style={{ width: "100%" }}>
       <Stack spacing={2}>
-        <SearchMenu categories={categories}/> 
+        <SearchMenu categories={categories} />
         <Button variant="contained">New product</Button>
-        <DataGrid rows={rows} columns={columns} checkboxSelection />
+        {products && (
+          <DataGrid rows={products} columns={columns} checkboxSelection />
+        )}
         <Statistics />
       </Stack>
     </div>
