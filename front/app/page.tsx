3b066290f955726fc4936ffc336b8e44c91fe82d
Milton Gonzalez <milton.gonzalez@ENCEDCCDMLT0498.local>
2025-01-02 00:47:33 -0600

    feat: Implement cell coloring

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
-import { DataGrid, GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
-import { Button, Modal, Stack } from "@mui/material";
+import {
+  DataGrid,
+  GridCellParams,
+  gridClasses,
+  GridColDef,
+  GridRowSelectionModel,
+} from "@mui/x-data-grid";
+import { Box, Button, Modal, Stack } from "@mui/material";
 import { SearchMenu } from "@/components/SearchMenu";
 import { Statistics } from "@/components/Statistics";
 import React, { useEffect } from "react";
@@ -8,7 +14,7 @@ import { getProducts, getCategories, getStatistics } from "@/utils/api";
 import { Statistic, Product } from "@/utils/types";
 import { ProductMenu } from "@/components/ProductMenu";
 import dayjs, { Dayjs } from "dayjs";
-import { darken, lighten, styled, Theme } from '@mui/material/styles';
+import { convertExpDate, StyledDataGrid } from "@/components/StyledDataGrid";
 
 const columns: GridColDef[] = [
   { field: "category", headerName: "Category", width: 150 },
@@ -16,7 +22,7 @@ const columns: GridColDef[] = [
   { field: "price", headerName: "Price", width: 150 },
   { field: "expDate", headerName: "Expiration Date", width: 200 },
   { field: "inStock", headerName: "Quantity in Stock", minWidth: 200 },
-  {field: "action", headerName: "Actions"}
+  { field: "action", headerName: "Actions" },
 ];
 
 const parseProducts: any = (products: Product[]) => {
@@ -50,24 +56,6 @@ const parseStats: any = (stats: Statistic[]) => {
   return parsed;
 };
 
-const today = dayjs();
-
-const convertExpDate: any = (date: Dayjs | null) => {
-  if (date === null) {
-    return 0  
-  }
-
-  let difference = date.diff(today, 'weeks');
-  console.log(difference);
-  if (difference > 2) {
-    return "OK"
-  } else if (difference <= 2 && difference >= 1) {
-    return "WARN"
-  } else {
-    return "ERR"
-  }
-}
-
 export default function Home() {
   const [products, setProducts] = React.useState([]);
   const [categories, setCategories] = React.useState<string[]>([]);
@@ -91,27 +79,37 @@ export default function Home() {
   };
   const handleFocusedChange = (newSelection: string[]) => {
     const unfocused = focused.filter((id: any) => !newSelection.includes(id));
-    const newlyFocused = newSelection.filter((id: any)=> focused.includes(id));
+    const newlyFocused = newSelection.filter((id: any) => focused.includes(id));
 
     console.log("Unfocused " + unfocused);
     console.log("Newly " + newlyFocused);
     unfocused.forEach((id) => console.log("Deselected " + id));
     newlyFocused.forEach((id) => console.log("Newly selected " + id));
 
-    setFocused(newSelection)
-  }
+    setFocused(newSelection);
+  };
 
   useEffect(() => {
-    getProducts(setProducts, parseProducts, "",[],"");
+    getProducts(setProducts, parseProducts, "", [], "");
     getStatistics(setStatistics, parseStats);
     getCategories(setCategories);
-  }, [modalOpen, editMenuOpen ]);
-
+  }, [modalOpen, editMenuOpen]);
 
   return (
     <div style={{ width: "100%" }}>
       <Stack spacing={2}>
-        <SearchMenu categories={categories}  getProducts={(name: any, categories: any, availability: any) => getProducts(setProducts, parseProducts, name, categories, availability)}/> 
+        <SearchMenu
+          categories={categories}
+          getProducts={(name: any, categories: any, availability: any) =>
+            getProducts(
+              setProducts,
+              parseProducts,
+              name,
+              categories,
+              availability
+            )
+          }
+        />
         <Button variant="contained" onClick={handleOpenModal}>
           New product
         </Button>
@@ -148,82 +146,58 @@ export default function Home() {
           />
         </Modal>
         {products && (
-          <StyledDataGrid
-            rows={products}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 10,
-                },
+          <Box
+            sx={{
+              [`.${gridClasses.cell}.OK`]: {
+                backgroundColor: "yellow",
+              },
+              [`.${gridClasses.cell}.WARN`]: {
+                backgroundColor: "red",
               },
             }}
-            checkboxSelection
-            disableRowSelectionOnClick
-            onRowSelectionModelChange={(e) => handleFocusedChange(e as string[])}
-            // onRowSelectionModelChange={(e, f) => {
-            //   setFocused(e);
-            // }}
-            onRowEditStart={(e) => console.log(e)}
-            onRowDoubleClick={(e: any) => {
-              setCurrentProduct(e.row);
-              handleEditMenu();
-            }}
-            getRowClassName={(params) => `super-app-theme--${convertExpDate(params.row.expDate)}`}
-          
-            rowSelectionModel={focused}
-          />
+          >
+            <StyledDataGrid
+              rows={products}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 10,
+                  },
+                },
+              }}
+              checkboxSelection
+              disableRowSelectionOnClick
+              onRowSelectionModelChange={(e) =>
+                handleFocusedChange(e as string[])
+              }
+              // onRowSelectionModelChange={(e, f) => {
+              //   setFocused(e);
+              // }}
+              onRowDoubleClick={(e: any) => {
+                setCurrentProduct(e.row);
+                handleEditMenu();
+              }}
+              getRowClassName={(params) =>
+                `super-app-theme--${convertExpDate(params.row.expDate)}`
+              }
+              getCellClassName={(params: GridCellParams<any, any, any>) => {
+                if (params.field === "inStock") {
+                  console.log(params);
+                  if (params?.value >= 5 && params.value <= 10) {
+                    return "OK";
+                  } else if (params?.value < 5) {
+                    return "WARN";
+                  }
+                }
+                return "";
+              }}
+              rowSelectionModel={focused}
+            />
+          </Box>
         )}
         <Statistics rows={statistics} />
       </Stack>
     </div>
   );
 }
-
-const getBackgroundColor = (color: string, theme: Theme, coefficient: number) => ({
-  backgroundColor: darken(color, coefficient),
-  ...theme.applyStyles('light', {
-    backgroundColor: lighten(color, coefficient),
-  }),
-});
-
-const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
-  '& .super-app-theme--OK': {
-    ...getBackgroundColor(theme.palette.success.main, theme, 0.5),
-    '&:hover': {
-      ...getBackgroundColor(theme.palette.success.main, theme, 0.4),
-    },
-    '&.Mui-selected': {
-      ...getBackgroundColor(theme.palette.success.main, theme, 0.5),
-      '&:hover': {
-        ...getBackgroundColor(theme.palette.success.main, theme, 0.4),
-      },
-    },
-  },
-  '& .super-app-theme--WARN': {
-    ...getBackgroundColor(theme.palette.warning.main, theme, 0.4),
-    '&:hover': {
-      ...getBackgroundColor(theme.palette.warning.main, theme, 0.2),
-    },
-    '&.Mui-selected': {
-      ...getBackgroundColor(theme.palette.warning.main, theme, 0.5),
-      '&:hover': {
-        ...getBackgroundColor(theme.palette.warning.main, theme, 0.4),
-      },
-    },
-  },
-  '& .super-app-theme--ERR': {
-    ...getBackgroundColor(theme.palette.error.main, theme, 0.7),
-    '&:hover': {
-      ...getBackgroundColor(theme.palette.error.main, theme, 0.6),
-    },
-    '&.Mui-selected': {
-      ...getBackgroundColor(theme.palette.error.main, theme, 0.5),
-      '&:hover': {
-        ...getBackgroundColor(theme.palette.error.main, theme, 0.4),
-      },
-    },
-  },
-}));
-
-
---
+++ front/components/StyledDataGrid.tsx
@@ -0,0 +1,68 @@
+import { darken, lighten, styled, Theme } from "@mui/material/styles";
+import { DataGrid } from "@mui/x-data-grid";
+import dayjs, { Dayjs } from "dayjs";
+
+const today = dayjs();
+
+export const convertExpDate: any = (date: Dayjs | null) => {
+  if (date === null) {
+    return 0;
+  }
+  let difference = date.diff(today, "weeks");
+  if (difference > 2) {
+    return "OK";
+  } else if (difference <= 2 && difference >= 1) {
+    return "WARN";
+  } else {
+    return "ERR";
+  }
+};
+const getBackgroundColor = (
+  color: string,
+  theme: Theme,
+  coefficient: number
+) => ({
+  backgroundColor: darken(color, coefficient),
+  ...theme.applyStyles("light", {
+    backgroundColor: lighten(color, coefficient),
+  }),
+});
+
+export const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
+  "& .super-app-theme--OK": {
+    ...getBackgroundColor(theme.palette.success.main, theme, 0.5),
+    "&:hover": {
+      ...getBackgroundColor(theme.palette.success.main, theme, 0.4),
+    },
+    "&.Mui-selected": {
+      ...getBackgroundColor(theme.palette.success.main, theme, 0.5),
+      "&:hover": {
+        ...getBackgroundColor(theme.palette.success.main, theme, 0.4),
+      },
+    },
+  },
+  "& .super-app-theme--WARN": {
+    ...getBackgroundColor(theme.palette.warning.main, theme, 0.4),
+    "&:hover": {
+      ...getBackgroundColor(theme.palette.warning.main, theme, 0.2),
+    },
+    "&.Mui-selected": {
+      ...getBackgroundColor(theme.palette.warning.main, theme, 0.5),
+      "&:hover": {
+        ...getBackgroundColor(theme.palette.warning.main, theme, 0.4),
+      },
+    },
+  },
+  "& .super-app-theme--ERR": {
+    ...getBackgroundColor(theme.palette.error.main, theme, 0.7),
+    "&:hover": {
+      ...getBackgroundColor(theme.palette.error.main, theme, 0.6),
+    },
+    "&.Mui-selected": {
+      ...getBackgroundColor(theme.palette.error.main, theme, 0.5),
+      "&:hover": {
+        ...getBackgroundColor(theme.palette.error.main, theme, 0.4),
+      },
+    },
+  },
+}));
